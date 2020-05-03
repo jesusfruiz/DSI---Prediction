eb23643961440c6975b0a63a096a3e0db373a842
Jesús Fernández-Bermejo Ruiz <jesus.fruiz@uclm.es>
2020-05-03 19:28:26 +0200

    Same predictions in all iterations

--- predictor.py
+++ predictor.py
@@ -6,8 +6,8 @@ Este es un archivo temporal.
 """
 
 import matlab.engine 
-import math
 import matplotlib.pyplot as plt
+from pylab import * # importar todas las funciones de pylab
 
 from sklearn.datasets import load_digits
 from sklearn.svm import SVC
@@ -16,16 +16,12 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import make_scorer, mean_absolute_error
 import numpy as np
 import pandas as pd
-import statistics
+import random
 
 def get_model(x_train, y_train, grid, score):
-    rf = RandomForestRegressor()
-    rf = GridSearchCV(rf, grid, cv = 5, verbose=1, scoring=score, n_jobs = -1)
+    rf = RandomForestRegressor(random_state=random.seed(0))
+    rf = GridSearchCV(rf, grid, cv = 5, verbose=1, scoring=score, n_jobs = 1)
     rf.fit(x_train, y_train)
-    print(rf.best_params_)
-    print(rf.scorer_)
-    print(rf.best_estimator_)
-    print("The best score is", rf.best_score_)
     return rf
 
 def score_function(y_pred, y_true):
@@ -33,30 +29,6 @@ def score_function(y_pred, y_true):
     result = np.divide(diff, y_true, out=np.zeros_like(diff), where=y_true!=0, dtype=np.float64)
     ret = np.mean(result)
     return ret
-
-def draw_validation_curve(train_scores, test_scores, param_range):
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
-
-    plt.title("Validation Curve with SVM")
-    plt.xlabel(r"$\gamma$")
-    plt.ylabel("Score")
-    plt.ylim(0.0, 1.1)
-    lw = 2
-    plt.semilogx(param_range, train_scores_mean, label="Training score",
-                 color="darkorange", lw=lw)
-    plt.fill_between(param_range, train_scores_mean - train_scores_std,
-                     train_scores_mean + train_scores_std, alpha=0.2,
-                     color="darkorange", lw=lw)
-    plt.semilogx(param_range, test_scores_mean, label="Cross-validation score",
-                 color="navy", lw=lw)
-    plt.fill_between(param_range, test_scores_mean - test_scores_std,
-                     test_scores_mean + test_scores_std, alpha=0.2,
-                     color="navy", lw=lw)
-    plt.legend(loc="best")
-    plt.show()
     
 def transform_matlab_data(matlab_data):
     data = {}
@@ -91,14 +63,18 @@ def predict_community_data(ccaa_data, grid, init, score):
         print("The prediction and the real results are the following in the var", var)
         print(pred)
         print(y_test)
+        plot([61,62,63,64,65,66,67], pred, [61,62,63,64,65,66,67], y_test)   # generar el gráfico de la función y=x   
+        show()
 
 vars_to_predict = ['DailyCases', 'Hospitalized', 'Critical', 'DailyDeaths', 'DailyRecoveries']
+random.seed(0)
+np.random.seed(0)
 
 eng = matlab.engine.start_matlab() 
 output, name_ccaa, iso_ccaa, data_spain = eng.HistoricDataSpain(nargout=4)
 
-n_estimators = [30, 60, 120, 300, 600, 1200]
-max_features = [1, 2, 3, 4, 5, 6, 7, 8]
+n_estimators = [5, 30, 60, 120, 300, 600]
+max_features = [3, 4, 5, 6, 7, 8]
 max_features.append("auto")
 max_features.append("sqrt")
 grid = {'n_estimators': n_estimators,
